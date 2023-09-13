513a8de8383762fa0bf908129c63c8428d34eb34
Akhilan777 <knakhilan@gmail.com>
2023-09-13 17:04:24 +0530

    Added-login-functionality-for-admin

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app, db
-from flask import jsonify
+from app.models import Admin
+from flask import request, jsonify
+from flask_login import current_user, login_user, logout_user
 from sqlalchemy import text
 
 @app.route('/')
@@ -14,3 +16,26 @@ def test_database_connection():
         return jsonify({'message' : 'Database connection successful', 'version' : version}), 200
     except Exception as e:
         return jsonify({'message' : 'Database connection failed', 'error' : str(e)}), 500
+    
+
+@app.route('/login', methods=['POST'])
+def login():
+    if current_user.is_authenticated:
+        return jsonify({'loggedIn' : True, 'username' : current_user.username}), 200
+    
+    username = request.json.get('username')
+    password = request.json.get('password')
+    remember_me = request.json.get('remember_me')
+    user = Admin.query.filter_by(username=username).first()
+
+    if user is None or not user.check_password(password=password):
+        return jsonify({'message' : 'Login failed', 'reason' : 'Invalid username or password'}), 401
+    
+    login_user(user, remember=remember_me)
+    return jsonify({'message' : 'Login successful'}), 200
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return jsonify({'message' : 'Logout Successful'}), 200
