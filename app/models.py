9043a6690f24335a9e4262a3b0dda104672af079
Akhilan777 <knakhilan@gmail.com>
2023-09-13 12:50:49 +0530

    Created migration and added admin table

--- app/models.py
+++ app/models.py
@@ -1,1 +1,21 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Admin(db.Model):
+    __tablename__ = "admin"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+    def __repr__(self):
+        return f"User id : {self.id}\nUser name : {self.name}\nUser username : {self.username}"
+    
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    
+    def check_password(self, password):
+        return check_password_hash(password)
